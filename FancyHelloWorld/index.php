14650ec6286e306ed53af3a3b805f26b3b1d66a6
Jake Scholl <j-scholl@onu.edu>
2017-01-26 20:06:11 -0500

    Add files via upload

---
+++ FancyHelloWorld/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Greeting to the University</title>
+        <link rel="stylesheet" href="Main.css">
+    </head>
+    <body>
+        Hello, ONU!
+    </body>
+</html>
