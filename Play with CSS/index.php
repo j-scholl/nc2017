14927a2a9c1d1b0fb3ecfed0a57465addc186d56
Jake Scholl <j-scholl@onu.edu>
2017-03-21 12:03:06 -0400

    Add files via upload

---
+++ Play with CSS/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Emblem of Awakening</title>
+        <link rel="stylesheet" href="Main.css">
+    </head>
+    <body>
+        <h1><center>Gemstones of the Shield of Seals</center></h1>
+        <center> The Fire Emblem is one of the national treasures of Ylisse and
+            a recurring sacred relic throughout the series.  In Fire Emblem:
+            Awakening, it appears as the Shield of Seals and bears multiple
+            gemstones.</center>
+        <div class="box" id="argent">
+            <center>Argent</center>
+            <div class="description">
+                Location: Halidom of Ylisse<br>
+                Owner: Exalt Emmeryn
+        </div>
+        </div>
+        <div class="box" id="gules">
+            <center>Gules</center>
+            <div class="description">
+                Location: Regna Ferox<br>
+                Owner: Khan Basilio
+            </div>
+        </div>
+        <div class="box" id="azure">
+            <center>Azure</center>
+            <div class="description">
+                Location: Mila Tree<br>
+                Owner: Priestess Tiki
+            </div>
+        </div>
+        <div class="box" id="vert">
+            <center>Vert</center>
+                <div class="description">
+                Location: Kingdom of Valm (formerly Chon'sin)<br>
+                Owner: Conqueror Walhart
+            </div>
+        </div>
+        <div class="box" id="sable">
+            <center>Sable</center>
+            <div class="description">
+                Location: Plegia<br>
+                Owner: King Gangrel
+            </div>
+        </div>
+    </body>
+</html>
