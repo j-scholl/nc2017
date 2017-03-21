26adae7e013345951fb3c7e3d5dc6edb9a7ce32a
Jake Scholl <j-scholl@onu.edu>
2017-03-21 12:04:29 -0400

    Add files via upload

---
+++ Mimic a Print Advertisement/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Spectrum Internet</title>
+        <link rel="stylesheet" href="Main.css">
+    </head>
+    <body>
+        <div id="Item1"></div>
+        
+        <div id="Item2">
+            Get <span id="bold">lightning-fast <span class="newline">
+                    Internet speeds </span></span> at an unbeatable price
+        </div>
+        
+        <div id="Item3">
+            <div class="SmallPrice" id="Dollar">
+                $
+            </div>
+            <div class="LargePrice">
+                29
+            </div>
+            <div class ="SmallPrice" id ="Details1">
+                99<span class="SmallText">/mo</span>
+            </div>
+            <div class ="SmallText" id="Details2">
+                for 12 mos<span class="newline">when bundled*</span>
+            </div>
+        </div>
+        
+        <div id="Item4">
+            <div id="SpectrumImage"></div>
+            <div id ="ShopButton">
+                SHOP NOW
+            </div>
+            <div class="SmallText" id="Restrictions">
+                Restrictions apply*
+            </div>
+        </div>
+    </body>
+</html>
